8b43a1bce903e7e1e517a0497d5c11be925373e6
Apple <apple@Apples-MacBook-Pro.local>
2024-11-20 11:54:54 +0700

    Implement useNavigate route for link menu

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,35 +1,41 @@
 import React from "react";
-import "./Layout.css"
-import logo_img from "../../asset/logo/logotech.png"
-function LayoutManage (){
-    return(
+import "./Layout.css";
+import logo_img from "../../asset/logo/logotech.png";
+import { useNavigate } from "react-router-dom";
+
+function LayoutManage() {
+    const navigate = useNavigate();
+    
+    const onClickMenu = (routerName) => {
+        navigate(routerName);
+    }
+
+    return (
         <div className="Main-Body">
-
             <div className="Logo-Header">
-
-            <div className="BrandLogo">
-                <img 
-                src={logo_img}
-                 alt="logoimg"     
-                 style={{width:50}}
-                 />
-            </div>
-            <div className="BrandName">
-                NIT Technology
-            </div>
-
+                <div className="BrandLogo">
+                    <img 
+                        src={logo_img} 
+                        alt="logoimg" 
+                        style={{ width: 50 }} 
+                    />
+                </div>
+                <div className="BrandName">
+                    NIT Technology
+                </div>
             </div>
 
             <div className="Menu">
                 <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/product">Product</a></li>
-                    <li><a href="/category">Category</a></li>   
-                    <li><a href="/about">About</a></li>
-
+                    <li onClick={() => onClickMenu("/")}>Home</li>
+                    <li onClick={() => onClickMenu("/product")}>Product</li>
+                    <li onClick={() => onClickMenu("/category")}>Category</li>
+                    <li onClick={() => onClickMenu("/about")}>About</li>
+                    <li onClick={() => onClickMenu("/login")}>Login</li>
                 </ul>
             </div>
         </div>
-    )
+    );
 }
+
 export default LayoutManage;
